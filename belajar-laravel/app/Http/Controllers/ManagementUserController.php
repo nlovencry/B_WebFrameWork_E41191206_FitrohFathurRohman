591382a7b0bdcea25955a77c37eb556432b87f16
Fathur Rohman <fathur.rahman12300@gmail.com>
2021-03-23 20:09:36 +0700

    push

---
+++ belajar-laravel/app/Http/Controllers/ManagementUserController.php
@@ -0,0 +1,11 @@
+<?php 
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ManagementUserController extends Controller
+{
+	//di sini isi controller Management User
+	}
+ ?>
